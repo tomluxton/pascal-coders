6caf71015ff0250c830b06a7c4792ebd70196914
tomluxton <ta.luxton@gmail.com>
2022-05-01 21:47:39 +1000

    Created explore (home) page and set up routing for it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Explore from "./pages/Explore";
 
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+    <Router>
+      <Routes>
+        <Route path = "/" element = {<Explore />}/>
+      </Routes>
+    </Router>
+    </>
+  );
 }
 
 export default App
