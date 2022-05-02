91d886c32d0ea2f32c38e85ffc5a241240c4297b
tomluxton <ta.luxton@gmail.com>
2022-05-02 15:28:48 +1000

    Added frontend no styling for sign in and sign up pages. Need to connect it to firebase now

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
-
+import {ReactComponent as ArrowRightIcon} from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg"
 
 function SignIn() {
 
@@ -13,9 +14,54 @@ function SignIn() {
 
   const navigate = useNavigate()
 
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]:e.target.value
+    }))
+  }
+
 
   return (
-    <div>SignIn</div>
+    <>
+      <div>
+        <header>
+          <p>Welcome Back!</p>
+        </header>
+
+        <form>
+          <input type="email" className="emailInput" placeholder='Email' id="email" value = {email} onChange = {onChange}/>
+
+          <div>
+            <input 
+              type = {showPassword ? "text" : "password"}
+              className = "passwordInput"
+              placeholder='Password'
+              id='password'
+              value ={password}
+              onChange = {onChange}  
+            />
+            <img src={visibilityIcon} alt="show password" className="showPassword" onClick={() => setShowPassword((prevState) => !prevState)}/>
+          </div>
+
+          <Link to = "/forgot-password" className='forgotPasswordLink'>Forgot Password</Link>
+          
+          <div>
+            <p>Sign In</p>
+            <button className='signInButton'>
+              <ArrowRightIcon fill = "#ffffff" width = '34px' height = '34px' />
+            </button>
+          </div>
+        </form>
+
+        {/*Put google OAuth here*/}
+        
+
+        <Link to="/sign-up" className='registerLink' >
+          Sign Up Instead
+        </Link>
+      </div>
+    </>
   )
 }
 
