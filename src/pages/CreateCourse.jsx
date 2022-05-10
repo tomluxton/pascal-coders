cf8abd0b29382aedad63a94c8706052b68624381
tomluxton <ta.luxton@gmail.com>
2022-05-11 09:01:14 +1000

    Setup create listing page

--- src/pages/CreateCourse.jsx
+++ src/pages/CreateCourse.jsx
@@ -1,8 +1,9 @@
-import {useState} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
 import {useNavigate} from 'react-router-dom'
 
 function CreateCourse() {
+  const [loading, setLoading] = useState(false)
   const [formData, setFormData] = useState({
     type: '',
     name: '',
@@ -10,7 +11,31 @@ function CreateCourse() {
     images: {}
   })
 
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const isMounted = useRef(true)
 
+  useEffect(() => {
+    if(isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if(user){
+          setFormData({...formData, userRef: user.uid})
+        } else {
+          navigate('/sign-in')
+        }
+      })
+    }
+
+    return() => {
+      isMounted.current = false
+    }
+  }, [isMounted])
+
+  if(loading) {
+    return (
+      <h3>Loading</h3>
+    )
+  }
 
   return (
     <div>CreateCourse</div>
