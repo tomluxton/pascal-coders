34bb2570cdaf898a537bed2d70fda44f15436c8b
tomluxton <ta.luxton@gmail.com>
2022-05-11 09:27:30 +1000

    continue working and got front end of create course working

--- src/pages/CreateCourse.jsx
+++ src/pages/CreateCourse.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect, useRef} from 'react'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
 import {useNavigate} from 'react-router-dom'
+import {Container, Row, Col} from 'react-bootstrap'
 
 function CreateCourse() {
   const [loading, setLoading] = useState(false)
@@ -10,6 +11,8 @@ function CreateCourse() {
     price: '',
     images: {}
   })
+
+  const {type, name, price, images} = formData
 
   const auth = getAuth();
   const navigate = useNavigate();
@@ -31,6 +34,27 @@ function CreateCourse() {
     }
   }, [isMounted])
 
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log(formData)
+  }
+
+  const onChange = e => {
+
+    if (e.target.files){
+      setFormData((prevState) => ({
+        ...prevState,
+        images:e.target.files
+      }))
+    }
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: e.target.value
+      }))
+    }
+  }
+
   if(loading) {
     return (
       <h3>Loading</h3>
@@ -38,7 +62,29 @@ function CreateCourse() {
   }
 
   return (
-    <div>CreateCourse</div>
+    <Container>
+      <Row>
+      <h1>Create a Course</h1>
+      </Row>
+      <form onSubmit={onSubmit}>
+        <Row>
+          <label>Name</label>
+          <input type = "text" id = "name" value = {name} onChange = {onChange} required/>
+        </Row>
+        <Row>
+          <label>Price</label>
+          <input type = "number" id = "price" value = {price} onChange = {onChange} required/>
+        </Row>
+        <Row>
+          <label>Images and Videos</label>
+          <input type = "file" id = "images" accept = '.jpg,.png,.jpeg' onChange = {onChange} multiple required/>
+        </Row>
+        <Row>
+        <button type = "submit">Create</button>
+        </Row> 
+      </form>
+      
+    </Container>
 
   )
 }
