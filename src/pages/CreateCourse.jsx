8f09cf3a5d14787c84ca7b4e294c29a18b9fcdd5
tomluxton <ta.luxton@gmail.com>
2022-05-12 14:10:16 +1000

    Upload new course works. Multiple files works. Drag and drop works

--- src/pages/CreateCourse.jsx
+++ src/pages/CreateCourse.jsx
@@ -2,6 +2,11 @@ import {useState, useEffect, useRef} from 'react'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
 import {useNavigate} from 'react-router-dom'
 import {Container, Row, Col} from 'react-bootstrap'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL  } from "firebase/storage";
+import {db} from '../firebase.config'
+import {v4 as uuidv4} from 'uuid'
+import {toast} from "react-toastify"
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
 
 function CreateCourse() {
   const [loading, setLoading] = useState(false)
@@ -34,9 +39,79 @@ function CreateCourse() {
     }
   }, [isMounted])
 
-  const onSubmit = e => {
+  const onSubmit = async (e) => {
     e.preventDefault()
+    setLoading(true)
+
     console.log(formData)
+
+
+    // firebase storage for images
+
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) =>{
+        const storage = getStorage()
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+        const storageRef = ref(storage, 'images/' + fileName)
+
+
+        const uploadTask = uploadBytesResumable(storageRef, image)
+
+
+        //https://firebase.google.com/docs/storage/web/upload-files
+        uploadTask.on('state_changed', 
+          (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused');
+                break;
+              case 'running':
+                console.log('Upload is running');
+                break;
+            }
+          }, 
+          (error) => {
+            // Handle unsuccessful uploads
+            reject(error);
+          }, 
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      })
+    }
+
+    const imgURLs = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch(()=> {
+      setLoading(false)
+      toast.error('image not uploaded')
+      return
+    })
+
+    const formDataCopy = {
+      ...formData,
+      imgURLs,
+      timestamp: serverTimestamp(),
+    }
+
+    delete formDataCopy.images
+    console.log(imgURLs)
+    console.log("done")
+
+    const docRef = await addDoc(collection(db, 'courses'), formDataCopy)
+    setLoading(false)
+
+    toast.success('Listing Saved successfully')
+    //navigate(`/category/${formDataCopy.type}/${docRef.id}`)
   }
 
   const onChange = e => {
